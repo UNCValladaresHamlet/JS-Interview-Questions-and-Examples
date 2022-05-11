d0a1922b51b0ac4feaa6f94754bb006a295fa42d
Hamlet Valladares <sigorahv@gmail.com>
2022-05-11 02:22:11 -0400

    added more examples

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 // typeof 3.14 // Returns "number"
 // typeof true // Returns "boolean"
 // typeof undefined // Returns "undefined"
-// typeof null // Returns "object" (kind of a bug in JavaScript)
+// typeof null // Returns "object" Object with empty value
 // typeof Symbol('symbol') // Returns Symbol
 // typeof 234567890123456789012345678901234567890n // Returns bigint
 
@@ -40,9 +40,50 @@
 
 ////What is the difference between "==" & "==="
 //They are both comparsion operators
-1 == "1"     // true, only checks if they are the same value
+// 1 == "1"     // true, only checks if they are the same value
 
-//strict equality
-1 === "1"    // false, checks value and data type left side is a number and right side is a string so brings back false
+// //strict equality
+// 1 === "1"    // false, checks value and data type left side is a number and right side is a string so brings back false
 
 
+//What is the difference between null and undefined?
+//both represent empty value
+//Undefined, Undefined means a variable has been declared but has not yet been assigned a value. When you define a variable with no value, Javascript gives it a value of undefined.
+//null, An object with empty value
+
+// let x = typeof(undefined)
+// console.log(x) //returns undefined
+
+// let y = typeof(null);
+// console.log(y) //returns object
+
+// ///////////////////////////////////////////////////////
+// //What is prototypal inheritance?
+// let car = function(model) { //constuctor
+//     this.model = model;
+// };
+// car.prototype.getModel = function() {
+//     return this.model;
+// }
+// let toyota = new car('toyota');
+// console.log(toyota.getModel());
+
+// let nissan  = new car('nissan');
+// console.log(nissan.getModel());
+
+
+//What is the difference between function declaration and function expression?
+
+// console.log(a())
+// console.log(b())
+function a() {
+    console.log('function declaration');
+};
+
+let b = function() {
+    console.log('function expression');
+}
+// console.log(a())
+// console.log(b())
+
+//Function expression is an anonymous function that is saved into the variable. Since it is saved to the variable, it behaves like a variable
